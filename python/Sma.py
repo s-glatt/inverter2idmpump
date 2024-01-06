beb8220aa7eabe69a5c872c2f386659b5918b099
s-glatt <67458573+s-glatt@users.noreply.github.com>
2024-01-06 15:55:45 +0000

    add sleep-timeout for modbus

--- python/Sma.py
+++ python/Sma.py
@@ -5,6 +5,7 @@ from pymodbus.client import ModbusTcpClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.payload import BinaryPayloadBuilder
+import time
 
 #-----------------------------------------
 # Routine to read a float    
@@ -23,11 +24,14 @@ def read(sma_ip, sma_port):
         client.connect()  
         # solar power stored in 30775, see:
         # https://files.sma.de/downloads/EDMx-Modbus-TI-de-16.pdf
+        time.sleep(1)
         solarpower = readint(client,30775,3)
         # energymeterpower_drawn stored in 30865
         # energymeterpower_feedin stored in 30867, see:
         # https://files.sma.de/downloads/HM-20-Modbus-NSD-TI-en-12.pdf
+        time.sleep(1)
         energymeterpower_drawn = readint(client,30865,3)
+        time.sleep(1)
         energymeterpower_feedin = readint(client,30867,3)
         return (solarpower, energymeterpower_drawn, energymeterpower_feedin)
   
